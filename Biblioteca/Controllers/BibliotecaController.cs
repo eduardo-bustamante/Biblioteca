12517f9c45a8305a0987341ce0509d2783f1e148
Eduardo Bustamante <edu.bustamante1989@gmail.com>
2023-12-01 19:51:12 -0300

    Peojeto Pessoal criado por mim

---
+++ Biblioteca/Controllers/BibliotecaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca.Controllers
+{
+    public class BibliotecaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Biblioteca/Models/BibliotecaModel.cs
+++ Biblioteca/Models/BibliotecaModel.cs
@@ -1,19 +1,26 @@
-﻿namespace Biblioteca.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca.Models
 {
     public class BibliotecaModel
     {
          public int Id { get; set; }
+        [Required(ErrorMessage = "Digite o Nome do Livro")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "Digite o Nome do Autor")]
         public string Autor { get; set; }
         public string Colecao { get; set; }
         public int Volume { get; set; }
+        [Required(ErrorMessage = "Digite o numero de páginas")]
         public int Paginas { get; set; }
+        [Required(ErrorMessage = "Digite o Idioma")]
         public string Idioma { get; set; }
         public int Ano { get; set; }
+        [Required(ErrorMessage = "Digite a Editora")]
         public string Editora { get; set; }
         public string Tema { get; set; }
         public string Capa { get; set; }
-        public DateTime DataCadastro { get; set; }
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
         public Boolean Emprestado { get; set; }
 
     }
